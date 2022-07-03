e3f895e9d75ca46deca1b73b2e6f455fe9776f82
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-03 19:32:58 +0700

    statistics inference fundamental

---
+++ normal_standard_distribution/main.py
@@ -0,0 +1,6 @@
+# normal probability distribusi
+# dalam normal probability distribution terdapat dua tipe distribution, yaitu
+# normal distribution
+# a normal distribution is a contrinous probability distribution for a random variable x
+# the graph of a normal distribution is called the normal curve
+# standard normal distribution
