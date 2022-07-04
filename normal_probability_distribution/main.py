c17362b1c41a8d91849cc98d44525e9da716de8a
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-04 14:01:31 +0700

    summary material normal probability distribution

--- normal_probability_distribution/main.py
+++ normal_probability_distribution/main.py
@@ -2,3 +2,5 @@
 # a. when a random variable x is normally distributed, we can find the probability that x will lie an interval by calculating the are under the normal curve for the interval
 # b. to find the are under any normal curve, first convert the upper and lower bounds of the interval to z-scores
 # c. then use the standard normal distribution to find the area
+
+# summary, jadi kita dapat menghitung nilai probability dari suatu normal distribution dengan cara membawa normal distribution menjadi standard normal distribution
