2e47f52b0de11a14e6d496f8d2a491cd0c649fa5
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-05 09:20:06 +0700

    material normal value distribution in finding z-score

---
+++ normal_distribution_value/main.py
@@ -0,0 +1,9 @@
+# normal distribution value
+
+# finding z-scores
+# we were given a normally distributed random variable x and we found the probability that x would lie in an interval by calculating the area under the normal curve for the interval
+# but what if you are given a probability and want to find a value
+
+# difference for cdf and pdf
+# pdf (probability density function) adalah sebuah fungsi yang diseadikan oleh scipy yang digunakan untuk mencari nilai z-score, if diketahui nilai probabilitynya
+# cdf (cumulative density funcation) adalah sebuah fungsi yang disediakan oleh scipy yang digunakan untuk mencari nilai probability, if diketahui nilai z-scorenya
