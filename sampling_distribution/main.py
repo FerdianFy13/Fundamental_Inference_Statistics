7e8c44166113aecc3d7c90dce59f550f6cf25f22
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-08 15:00:32 +0700

    sampling distribution material

---
+++ sampling_distribution/main.py
@@ -0,0 +1,9 @@
+# sampling distributions and the central limit theoreom
+
+# sampling distribution
+# in this section, we will strudy the relationship between a population mean and the means of sample taken from the population
+# a sampling distribution is the probability distribution of a sample statistics that is formed when samples of size n are repeatedly form a populations
+# if the sample statistics is the sample mean, then the distribution is the sampling distribution of sample means
+# every sample statics has a sampling distribution
+
+# summary, sample distribution akan melakukan kalkulasi dari mean dari tiap sampling untuk mendaptakn mean dari sampling distribution dari sample n yang different
