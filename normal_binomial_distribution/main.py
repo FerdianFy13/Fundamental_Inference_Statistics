334b11e33ea00f0216c3f960aee40d6c3f657165
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-12 09:52:43 +0700

    approximating binomial probabilities

--- normal_binomial_distribution/main.py
+++ normal_binomial_distribution/main.py
@@ -12,3 +12,17 @@
 # remember that each bar has a width of one unit and x is the midpoint of the interval
 # when you use a continuous normal distribution to approximate a binomial probability, you need to move 0.5 unit to the left and right of the midpoint to include all possible x-values in the interval
 # when you do this, you are making a continuity correction
+
+# approximating binomial probabilities
+# verify that a binomial distibution
+# . specify n, p, and q
+# determine whether you can use a normal distribution to approximate x, the binomial variable
+# . is np >= 5 and nq >= 5
+# find the mean u and standard deviation sigma for the distribution
+# . u = np
+# apply the appropriate continuity correction. shade the corresponding area under the normal curve
+# . add 0.5 to (or substract 0.5 from) the binomual probabilities
+# find the correspoding z-score
+# . x = x - u / sigma
+# find the probabilities 
+# . using the standard normal table, z-score calculator, or scipy
