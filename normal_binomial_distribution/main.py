95fe72286ac63aee6a3503f82faaf70db8607345
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-12 09:17:23 +0700

    normal approximating a binomial distribution

---
+++ normal_binomial_distribution/main.py
@@ -0,0 +1,6 @@
+# normal binomial distribution
+
+# approximating a binomial distribution
+# . if np >= 5 and nq >= 5, then the binomial random variabke x is approximately normally distributed, with
+# mean: u = np
+# standard deviation sigma = akara npq
