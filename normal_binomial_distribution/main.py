08f14df72134c399fd8f2b8ad1635c0427d77e76
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-12 09:39:21 +0700

    continuity correction

--- normal_binomial_distribution/main.py
+++ normal_binomial_distribution/main.py
@@ -4,3 +4,11 @@
 # . if np >= 5 and nq >= 5, then the binomial random variabke x is approximately normally distributed, with
 # mean: u = np
 # standard deviation sigma = akara npq
+# nilai q dapat dicari dengan cara => 1 - dengan nilai dari p
+# sedangkan untuk nilai p dapat dicari dengan cara => 1 - dengan nilai dari q
+
+# continuty correction
+# a binomial distribution is discrete and can be represented by a probability histogram
+# remember that each bar has a width of one unit and x is the midpoint of the interval
+# when you use a continuous normal distribution to approximate a binomial probability, you need to move 0.5 unit to the left and right of the midpoint to include all possible x-values in the interval
+# when you do this, you are making a continuity correction
