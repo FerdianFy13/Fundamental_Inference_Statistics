f6902f40ea8939dad134fd486a4d680ac4a9e833
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-13 14:57:31 +0700

    point estimate

--- confidence_interval/main.py
+++ confidence_interval/main.py
@@ -5,3 +5,9 @@
 # in this chapter, we will learn an important technique or statistical inference to use sample statistics to estimate the value of an unknown population paramter
 # in this section and the next, we will learn how to use sample statistics to make an estimate of the population parameter u when the population standard deviation sigma is known this section or when sigma is unknown
 # to make such an inference, begin by finding a point estimate
+
+# * point estimate
+# a point estimate is a single value estimate for a population parameter
+# the most unbiased point estimate of the population mean u is the sample mean x bar
+# the validity of an estimation method is increased when you use a sample statistics that is unbiased and has low variability
+# a sample statistics is unbiased if it does not overestimate or underestimate the populations parameter
