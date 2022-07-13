fd95769ce9b0f6ca02aab789e10b55d940703ed0
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-13 14:43:38 +0700

    confidence interval for the mean, if sigma known

---
+++ confidence_interval/main.py
@@ -0,0 +1,7 @@
+# confidence interval
+
+# 6.1 confidence intervals for the mean (sigma known)
+# * estimating population parameters
+# in this chapter, we will learn an important technique or statistical inference to use sample statistics to estimate the value of an unknown population paramter
+# in this section and the next, we will learn how to use sample statistics to make an estimate of the population parameter u when the population standard deviation sigma is known this section or when sigma is unknown
+# to make such an inference, begin by finding a point estimate
