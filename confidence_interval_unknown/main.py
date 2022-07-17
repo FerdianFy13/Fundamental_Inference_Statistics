19678cc2c1bfeb5733e13c1c14d40f230db57120
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-17 18:50:04 +0700

    the t-distribution in confidence interval unknown

---
+++ confidence_interval_unknown/main.py
@@ -0,0 +1,6 @@
+# confidence interval for the mean (sigma unknown)
+
+# * the t-distribution
+# in many real life situations, the populations standard deviation sigma is unknown
+# so, how can you construct a confidence interval for a population mean when sigma is nott known
+# for a random variable that is normally distributed (or approximately normally distributed), you can use a t-distribution
