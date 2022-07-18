7cf2d268b0756458d7175a92656863e13b704841
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-18 14:06:37 +0700

    constructing interval for a populations proportion

--- confidence_interval_proportion/main.py
+++ confidence_interval_proportion/main.py
@@ -12,3 +12,12 @@
 # where x is the number of successes in the sample and n is the sample size
 # the point estimate for the populations proportion of failure is q = 1 - p
 # the symbols  p^ and q^ are read as p hat and q hat
+
+# * confidence intervals for a populations proportion
+# constructing a confidence interval for a populations proportion p is similar to constructing a confidence interval for a populations mean
+# we start with a point estimate and calculate a margin of error
+# a confidence interval for a population proportion p is:
+# . p^ - E < p < p^ + E
+# where margin of error for p is:
+# . E = zc sqrt p^q^/n
+# the probability that the confidence interval contains p is c, assuming that the estimation process is repeated a large number of times.
