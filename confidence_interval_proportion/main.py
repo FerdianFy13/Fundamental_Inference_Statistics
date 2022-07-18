44be303f89b9c6cd11b73c3ddbab00a022909e30
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-18 14:00:06 +0700

    point estimate for p

--- confidence_interval_proportion/main.py
+++ confidence_interval_proportion/main.py
@@ -5,3 +5,10 @@
 # this probability is a population proportion
 # in this section, we will learn how to estimate a population proportion p using a confidence interval
 # as with confidence intervals for x, we will start a a point estimate
+
+# * point estimate for p
+# the point estimate for p, the population proporion of successses is given by the proportion of success in a smaple and is denoted by
+# . p = x / n
+# where x is the number of successes in the sample and n is the sample size
+# the point estimate for the populations proportion of failure is q = 1 - p
+# the symbols  p^ and q^ are read as p hat and q hat
