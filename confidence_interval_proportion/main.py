9af3e558a04ceaef389fb4e18c0c9066eb424854
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-18 13:47:55 +0700

    point estimate for a population proportion

---
+++ confidence_interval_proportion/main.py
@@ -0,0 +1,7 @@
+# confidence intervals for populations proportions
+
+# * point estimate for a population proportion
+# the probability of success in a single trial of a binomial experiment is p
+# this probability is a population proportion
+# in this section, we will learn how to estimate a population proportion p using a confidence interval
+# as with confidence intervals for x, we will start a a point estimate
