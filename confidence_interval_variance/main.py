be674b459d5a7663615938759b02171b73390765
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-23 08:21:04 +0700

    the chi-square distribution

--- confidence_interval_variance/main.py
+++ confidence_interval_variance/main.py
@@ -6,3 +6,8 @@
 # it is important that the parts vary little or not at all
 # we can start with a point estimate
 # the pointer estimate for sigma2 is s2 and the point estimate for sigma is s
+
+# * the chi-square distribution
+# we can use a chi-square distribution to construct a confidence interval for the variance and standard deviation
+# if a random variable x has a nonrmal distributionm the then distribution of x2 forms a chi-square distribution for samples of any size n > 1 
+# . x2 = (n - 1)s2 / sigma2
