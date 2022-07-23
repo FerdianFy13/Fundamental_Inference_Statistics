7b5b14bb1e647f58e0a0d73c0962a1ad78d63501
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-23 08:08:40 +0700

    point estimate for confidence_interval_variance

---
+++ confidence_interval_variance/main.py
@@ -0,0 +1,8 @@
+# confidence interval for variance and standard deviation
+
+# * point estimate for variance and standard deviation
+# in manufacturing it is necessary to control the amount that a process varies
+# for instance an automobile part manufacture must produce thousands of parts to be used in the manufacturing process
+# it is important that the parts vary little or not at all
+# we can start with a point estimate
+# the pointer estimate for sigma2 is s2 and the point estimate for sigma is s
