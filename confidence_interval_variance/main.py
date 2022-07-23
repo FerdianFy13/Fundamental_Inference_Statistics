8fffa3730ba41f0a9c7830280d17a3bb2806f217
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-23 15:04:40 +0700

    cancel interpretation finding critical value

--- confidence_interval_variance/main.py
+++ confidence_interval_variance/main.py
@@ -9,7 +9,7 @@
 
 # * the chi-square distribution
 # we can use a chi-square distribution to construct a confidence interval for the variance and standard deviation
-# if a random variable x has a nonrmal distributionm the then distribution of x2 forms a chi-square distribution for samples of any size n > 1 
+# if a random variable x has a nonrmal distributionm the then distribution of x2 forms a chi-square distribution for samples of any size n > 1
 # . x2 = (n - 1)s2 / sigma2
 
 
@@ -20,22 +20,3 @@
 # the total are under each chi-square distribution curve is equal to 1
 # the chi-square distribution is positively skewed and therefore the distribution is not symmetric
 # the chi-square distribution is different for each number of degrees of freedom as shown in the figure as the degees of freedom increase the chi-square distribution approaches a normal distribution
-
-from scipy.stats import chi2
-from math import sqrt
-
-n = 18
-df = n-1
-c = 0.95
-
-xr2 = (1-c)/2
-xl2 = (1+c)/2
-
-print(f'xr2: {xr2}')
-print(f'xl2: {xl2}')
-print()
-
-xr2_critical = chi2.ppf(1-xr2, df)
-xl2_critical = chi2.ppf(1-xl2, df)
-print(f'xr2_critical: {xr2_critical}')
-print(f'xl2_critical: {xl2_critical}')
