dadacfa4cf93d02612c3f4c51a0e1c7e02b4154a
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-24 19:57:48 +0700

    hypotesis testing

---
+++ hypotesis_testing/main.py
@@ -0,0 +1,5 @@
+# Hypothesis Testting
+
+# * Hypothesis tests
+# a hypothesis test is a process that uses sample statistics to test a claim about the value of a population parameter
+# researches in field such as medicine, psychology and businees relu on hypothesis testing to make informed decisions about new medicine, treatments, and marketing strategies
