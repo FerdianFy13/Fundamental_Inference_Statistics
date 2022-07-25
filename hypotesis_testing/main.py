365b1738073404dd1c9c96082008acdc406e302b
FerdianFy13 <ferdianfy13@gmail.com>
2022-07-25 08:37:46 +0700

    starting a hypothesis

--- hypotesis_testing/main.py
+++ hypotesis_testing/main.py
@@ -3,3 +3,10 @@
 # * Hypothesis tests
 # a hypothesis test is a process that uses sample statistics to test a claim about the value of a population parameter
 # researches in field such as medicine, psychology and businees relu on hypothesis testing to make informed decisions about new medicine, treatments, and marketing strategies
+
+# * starting a hypothesis
+# a statement about a population parameter is called a statistical hypothesis
+# to test a population paramter, tou should carefully state a pair of hypotheses one tahta represents the claim and the other its complement
+# when one of these hypotheses is false, the other must be true. either hypothesis the null hypothesis or the alternative hypothesis may represents the original claim
+# the term null hypothesis was introduced by ronald fisher
+# if the statement in the null hypothesis is not true, then the alternative hypothesis must be true
